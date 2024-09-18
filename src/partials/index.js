9386c5f883d100a8911901a52928c553459a793a
Rayyan Sadiq <raysadik5@gmail.com>
2024-09-17 22:00:49 -0500

    first commit

---
+++ src/partials/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+export {
+    Hero,
+    Footer,
+    CustomerReviews,
+    Services,
+    PopularProducts,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe
+};
